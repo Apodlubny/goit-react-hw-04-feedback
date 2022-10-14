ad986d89f8795615548177193dbf0254588f0757
Apodlubny <apodlubny@gmail.com>
2022-10-14 15:33:54 +0300

    copy HW-2-feedback

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 50px auto;
+  width: 400px;
+  border: 1px solid black;
+  border-radius: 6px;
+  box-shadow: 1rem 1rem 2rem rgb(65, 64, 64), -1rem -1rem 2rem rgb(82, 81, 81);
+  background-color: #f5efef;
+`;
---
+++ src/components/Notification/Notification.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const NotificationMessage = styled.p`
+  margin-top: 0;
+  color: #e79610;
+  margin-left: auto;
+  margin-right: auto;
+  font-weight: 600;
+  text-align: center;
+`;
